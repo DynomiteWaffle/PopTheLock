2ecae7882b0acb623d68df98e71853d7a4adc49b
DynomiteWaffle <dynomitewaffle@protonmail.com>
2025-10-05 18:00:42 -0600

    resembling a game

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image/color"
+	"math"
 	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,13 +13,17 @@ import (
 
 type Game struct {
 	score         int
-	pickPositon   int //rotation
-	pinPosition   int // 8 states - 45 deg appart
+	pickPositon   float64 //rotation
+	pickSpeed     float64
+	pinPosition   float64 // 8 states - 45 deg appart
 	pinMaxCount   int
 	degreeFreedom float32 // amount of error you can have on input
-	debugText     string
-	debugshow     bool
-	debugAuto     bool
+	gameOver      bool
+
+	debugText string
+	debugshow bool
+	debugAuto bool
+
 	// window size
 	windowWidth        int
 	windowCenterWidth  int
@@ -38,6 +43,9 @@ func (g *Game) Update() error {
 	g.debugText = ""
 	// window size
 	g.debugText += "Window:\nSize " + strconv.Itoa(g.windowHeight) + "x" + strconv.Itoa(g.windowWidth) + "\nUnit " + strconv.Itoa(g.windowUnit) + "\n"
+	g.debugText += "\nPositions\n"
+	g.debugText += "Pick " + strconv.FormatFloat(g.pickPositon, 'f', 2, 64) + "\n"
+	g.debugText += "Pin " + strconv.FormatFloat(g.pinPosition, 'f', 2, 64) + "\n"
 
 	g.debugText += "\nMisc:\n"
 	g.debugText += "Score " + strconv.Itoa(g.score) + "\n"
@@ -97,6 +105,21 @@ func (g *Game) Update() error {
 	// activate pin - mouse click or keyboard space
 	if inpututil.IsKeyJustPressed(ebiten.KeySpace) || inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0) {
 		g.debugText += "Clicked\n"
+		// hit pin
+		if math.Abs(g.pickPositon-g.pinPosition) <= float64(g.degreeFreedom) {
+			g.score++
+		} else {
+			// missed
+			g.gameOver = true
+		}
+	}
+	// went over pin
+	if g.pickPositon > g.pinPosition+float64(g.degreeFreedom) {
+		g.gameOver = true
+	}
+	// move pick
+	if !g.gameOver {
+		g.pickPositon += g.pickSpeed
 	}
 
 	return nil
@@ -120,6 +143,9 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	// TODO draw pick
 	// TOOD draw pin
+	pindist := 0.31
+	vector.DrawFilledCircle(screen, float32(math.Cos(deg2rad(g.pinPosition))*float64(g.windowUnit)*pindist)+float32(g.windowCenterWidth), (float32(math.Sin(deg2rad(g.pinPosition))*float64(g.windowUnit))*float32(pindist))+float32(g.windowUnit)*2, float32(g.windowUnit)/40, color.Black, true)
+	vector.DrawFilledCircle(screen, float32(math.Cos(deg2rad(g.pickPositon))*float64(g.windowUnit)*pindist)+float32(g.windowCenterWidth), (float32(math.Sin(deg2rad(g.pickPositon))*float64(g.windowUnit))*float32(pindist))+float32(g.windowUnit)*2, float32(g.windowUnit)/40, color.Black, true)
 	// TODO draw score - with text.draw() - needs font
 }
 func (g *Game) Layout(outWidth, outHeight int) (screenWidth, screenHeight int) {
@@ -145,13 +171,16 @@ func main() {
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	game := &Game{}
 	game.pinMaxCount = 8
-	game.degreeFreedom = 1
+	game.degreeFreedom = 3.5
 	// colors
 	game.lockBodyColor = color.RGBA{A: 255}
 	game.lockShackleColor = color.RGBA{R: 100, B: 100, G: 100, A: 255}
 	game.lockDialColor = color.RGBA{R: 255, B: 255, G: 255, A: 255}
 
 	game.debugshow = true //TODO remove
+	game.pinPosition = 90
+	game.pickPositon = -90
+	game.pickSpeed = 1.1
 	ebiten.RunGame(game)
 }
 
@@ -163,4 +192,8 @@ func toggleBool(rule bool, item *bool) {
 			*item = true
 		}
 	}
+}
+
+func deg2rad(degree float64) float64 {
+	return (math.Pi / 180) * degree
 }
